0009b861da53d8d0ff12e33ea35b491647beff8a
ioan <costeaioan23@gmail.com>
2020-11-04 20:16:14 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Costea_Ioan_Lab5
+{
+    enum ActionState
+    {
+        New,
+        Edit,
+        Delete,
+        Nothing
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ActionState action = ActionState.Nothing;
+        PhoneNumbersDataSet phoneNumbersDataSet = new PhoneNumbersDataSet();
+        PhoneNumbersDataSetTableAdapters.PhoneNumbersTableAdapter tblPhoneNumbersAdapter = new PhoneNumbersDataSetTableAdapters.PhoneNumbersTableAdapter();
+        Binding txtPhoneNumberBinding = new Binding();
+        Binding txtSubscriberBinding = new Binding();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            grdMain.DataContext = phoneNumbersDataSet.PhoneNumbers;
+            txtPhoneNumberBinding.Path = new PropertyPath("Phonenum");
+            txtSubscriberBinding.Path = new PropertyPath("Subscriber");
+            txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+            txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
+
+        }
+
+        private void lstPhonesLoad()
+        {
+            tblPhoneNumbersAdapter.Fill(phoneNumbersDataSet.PhoneNumbers);
+        }
+
+        
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void grdMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            lstPhonesLoad();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    namespace PhoneBook
+    {
+        /// <summary>
+        /// Interaction logic for MainWindow.xaml
+        /// </summary>
+        /// 
+
+        
+        public partial class MainWindow : Window
+        {
+            
+
+           
+        }
+    }
+}
